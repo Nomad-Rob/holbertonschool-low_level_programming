839d85042da09fee05847ab37445420f18d99d36
Nomad-Rob <rfarley89@gmail.com>
2023-01-26 11:12:53 -0800

    fixed trailing whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,26 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h> 
+#include<stdlib.h>
+#include<time.h>
+#include <stdio.h>
 
 /**
- * 
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	int n;
-	int x;
+	int n, r;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-	printf("Last digit of %d is %d ", n, x);
-	if (x > 5)
-		printf("and is greater than 5\n");
-	if (x == 0)
-		printf("and is 0\n");
-	if ((x < 6) && (x != 0))
-		printf("and is less than 6 and not 0\n");
+	r = n % 10;
+
+	if (r > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, r);
+	else if (r == 0)
+		printf("Last digit of %i is %i and is 0\n", n, r);
+	else if (r < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, r);
 
 	return (0);
 }
