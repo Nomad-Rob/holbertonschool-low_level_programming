d90f87a530c23db685fb40d53a5f79aef62f395a
Nomad-Rob <rfarley89@gmail.com>
2023-02-13 10:31:27 -0800

    added 8??

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - printsd the sum of the two diagonals of a square matrix
+ * @a: multi-dimesional array to get sums of
+ * @size: size top to bottom, left to right, and diagonal
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+	int i = 0;
+	int j = size - 1;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	while (i < size)
+	{
+		sum1 += a[i];
+		sum2 += a[j];
+		a += size;
+		j--;
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
