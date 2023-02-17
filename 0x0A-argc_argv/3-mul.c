e3b9f85f6e4fa08ee9daa5d15f450cc846c0b8cc
Nomad-Rob <rfarley89@gmail.com>
2023-02-17 12:36:47 -0800

    adding 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguements
+ * @argv: numbers to multiply
+ * Return: 0
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+
+	if (argc - 1 <= 1)
+	{
+		printf("Error\n");
+		return  (1);
+	}
+
+	num1 = atoi(argv[argc - 2]);
+	num2 = atoi(argv[argc - 1]);
+	printf("%d\n", num1 * num2);
+	return (0);
+}
