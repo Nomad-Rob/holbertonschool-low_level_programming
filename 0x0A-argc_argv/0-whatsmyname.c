88d6d5099aa5a6cf81ee3dca03533f67a8df332b
Nomad-Rob <rfarley89@gmail.com>
2023-02-17 09:40:53 -0800

    adding readme, main, 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+* main - prints name with \n
+* @argc: number of arguments to be counted
+* @argv: array pointing to arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
