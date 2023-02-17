4303f1cba6754b98d3991f02910589569cefe065
Nomad-Rob <rfarley89@gmail.com>
2023-02-17 12:28:53 -0800

    adding 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+/**
+* main - 
+* argc: counts the number of arguments
+* argv: array of strings
+*
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (argv[i] != argv[argc])
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
