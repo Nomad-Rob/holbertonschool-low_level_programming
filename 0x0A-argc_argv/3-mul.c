a2d67d1a0bc40e4977c110d71214dd4524de3462
Nomad-Rob <rfarley89@gmail.com>
2023-02-17 12:45:51 -0800

    adding 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
  * main - multiplies two numbers
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - adds two numbers
+ * @argc: number of arguements
+ * @argv: numbers to add are index 1 and 2
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	i = 1;
+	sum = 0;
+	while (argv[i] != argv[argc])
+	{
+		if  (atoi(argv[i]) == '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
