5c0d176cbce0d33dc4a1c6238560bd10021e1bed
Nomad-Rob <rfarley89@gmail.com>
2023-02-23 11:20:21 -0800

    fixing 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+
 /**
- * main - prints name of the file it was compiled from
- *
- *
- * Return: 0 if successful
- *
+ * main - prints out the name of its source file
+ * Return: always 0
  */
 
 int main(void)
