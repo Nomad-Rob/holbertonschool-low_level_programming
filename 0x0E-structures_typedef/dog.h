8aaf6717f4f4f2222e4b6b9703d32296d34cfcfe
Nomad-Rob <rfarley89@gmail.com>
2023-03-01 08:35:39 -0800

    adding exe file a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
+/* structs & typedefs */
 /**
- * init_dog - initializes a variable of the type dog
- * @d: struct to use
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of the dog
- *
+ * struct dog - structure for dog that contain name,age, and owner variables
+ * @name: string containing dog's name
+ * @age: float containing dog's age
+ * @owner: string containing dog's owner's name
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (!d)
-		return;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+/* function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
