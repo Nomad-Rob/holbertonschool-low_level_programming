443b488842edecbbc585ca7e000e40ecc2ac0300
Nomad-Rob <rfarley89@gmail.com>
2023-03-20 14:44:44 -0700

    Output issue fixed, rewrote code from scratch

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,34 @@
 #include "main.h"
 
 /**
-* read_textfile - reads a text file and prints it to stdout
-* @filename: name of text file
-* @letters: number of letters it should read and print
-*
-* Return: 0 if file can not be opened or read, filename is NULL
-* 0 if write fails or does not write expected byte amount
-* else actual number of letters it could read and print
-*/
+ * read_textfile - reads a text file and prints it to the stdout
+ * @filename: file to be opened and printed
+ * @letters: number of letter it should read and print
+ * Return: 0 if fails or if write fails or number of letters it read and print
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, n;
+	int fd;
+	ssize_t n = 0;
 	char *buf;
 
-	if (filename == NULL)
-		return (0);
-
-	o = open(filename, O_RDWR);
-
-	if (o == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
+	buf = malloc(letters);
+	if (buf == NULL || filename == NULL)
 	{
-		close(o);
+		free(buf);
 		return (0);
 	}
-
-	read(o, buf, letters);
-
-	buf[letters] = '\0';
-
-	n = write(STDOUT_FILENO, buf, letters);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	n = read(fd, buf, letters);
 	if (n == -1)
 		return (0);
-
-	close(o);
+	n = write(STDOUT_FILENO, buf, n);
+	if (n == -1)
+		return (0);
 	free(buf);
-
+	close(fd);
 	return (n);
 }
