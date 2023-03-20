38178cfa7177c4499965bbcb997ba7407ad3d998
Nomad-Rob <rfarley89@gmail.com>
2023-03-20 14:42:36 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.  ALSO added Requiescat file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to stdout
+* @filename: name of text file
+* @letters: number of letters it should read and print
+*
+* Return: 0 if file can not be opened or read, filename is NULL
+* 0 if write fails or does not write expected byte amount
+* else actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int o, n;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	o = open(filename, O_RDWR);
+
+	if (o == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+	{
+		close(o);
+		return (0);
+	}
+
+	read(o, buf, letters);
+
+	buf[letters] = '\0';
+
+	n = write(STDOUT_FILENO, buf, letters);
+	if (n == -1)
+		return (0);
+
+	close(o);
+	free(buf);
+
+	return (n);
+}
