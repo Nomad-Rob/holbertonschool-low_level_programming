f6c19e015f1b4823d47dc5a363d16f272aabf1b3
Nomad-Rob <rfarley89@gmail.com>
2023-03-20 15:25:28 -0700

    Write a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * main - copies a file to another
+ * @argc: number of arguements
+ * @argv: arguements themselves
+ * Return: 0 if succesful and -1 if not
+ */
+
+int main(int argc, char **argv)
+{
+	int srcd, dest, nchars;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+		exit(97);
+	nchars = 1024;
+
+	srcd = open(argv[1], O_RDONLY);
+	if (srcd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		exit(98);
+	dest = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (dest == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+		exit(99);
+	while (nchars != 0)
+	{
+		nchars = read(srcd, buf, 1024);
+		if (nchars == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+		if (write(dest, buf, nchars) != nchars)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
+	}
+	if (close(dest) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", dest),
+		exit(100);
+	if (close(srcd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", srcd),
+		exit(100);
+	return (0);
+}
